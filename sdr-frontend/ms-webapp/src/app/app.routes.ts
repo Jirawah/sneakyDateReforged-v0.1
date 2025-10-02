95a69e72b96373c23df189ad0fc8e4afcadeb87d
Jirawah <antoine.pouzeratte@hotmail.fr>
2025-10-02 23:26:46 +0200

    ms-webapp created, working on the frontend architecture, adding dependencies, next step, creating the components

--- sdr-frontend/ms-webapp/src/app/app.routes.ts
+++ sdr-frontend/ms-webapp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+  // plus tard : /profile, /rdv, /invitation, /friend, /notif...
+];
